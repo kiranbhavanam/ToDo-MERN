795aa84d35b53a17cf6616a19877122506bc4857
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-12-26 12:44:59 +0530

    defining validation using zode

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,11 @@
 const express=require("express");
 const app=express();//initializes empty express app.
 app.use(express.json());//make sures all the post endpoints will work,parse the body if its json body.
-
+//Authentication for signup and signin
 app.post("/signup",(req,res)=>{})
 app.post("/signin",(req,res)=>{})
 
-
+// input validation for all the routes.
 app.get("/todos",(req,res)=>{})
 app.post("/newTodo",(req,res)=>{})
 app.put("updateToDo",(req,res)=>{})
---
+++ backend/types.js
@@ -0,0 +1,28 @@
+//input validation using zod.
+
+const zod=require("zod");
+/**
+    for /newTodo:
+    {
+        title:string,
+        description:string
+    }
+    
+    for  /updateTodo:
+    {
+        id:string
+    }
+ */
+
+    const newTodoSchema=zod.object(
+        {title:zod.string(),
+            description:zod.string()
+        }
+    );
+    const updateTodoSchema=zod.object({
+        id:zod.string()
+    });
+
+module.exports={
+    newTodoSchema,updateTodoSchema
+}
