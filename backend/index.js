21c6795875702ec171f57f993b6858319ba4af91
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-12-26 12:57:24 +0530

    perform input validation

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 //with express.json() middleware.
 
 const express=require("express");
+const { newTodoSchema,updateTodoSchema } = require("./types");
 const app=express();//initializes empty express app.
 app.use(express.json());//make sures all the post endpoints will work,parse the body if its json body.
 //Authentication for signup and signin
@@ -10,8 +11,28 @@ app.post("/signin",(req,res)=>{})
 
 // input validation for all the routes.
 app.get("/todos",(req,res)=>{})
-app.post("/newTodo",(req,res)=>{})
-app.put("updateToDo",(req,res)=>{})
+app.post("/newTodo",(req,res)=>{
+    const createPayLoad=req.body;
+    const parsePayLoad=newTodoSchema.safeParse(createPayLoad);
+    if(!parsePayLoad.success){
+        res.status(411).json(
+            {msg:"You sent the wrong inputs"}
+        )
+        return;
+    }
+    // put it in the mongoDB.
+})
+app.put("updateToDo",(req,res)=>{
+    const updatePayLoad=req.body;
+    const parsePayLoad=updateTodoSchema.safeParse(updatePayLoad);
+    if(!parsePayLoad.success){
+        res.status(411).json({
+            msg: "there is no such todo"
+        })
+        return;
+    }
+    //update todo in mongoDB.
+})
 
 app.listen(3000,(error)=>{
     console.log(error);
