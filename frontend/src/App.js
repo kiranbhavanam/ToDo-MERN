788aa3814f683fc2876973d981b8e3e1ac0da61d
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-12-26 19:08:09 +0530

    creating frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { CreateTodo } from './components/CreateTodo';
+import { Todo } from './components/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      hi there
+      <CreateTodo/>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
