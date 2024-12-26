2e7dc121ae254058384bbdd2ed81ff289c09448f
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-12-26 23:28:10 +0530

    Frontend of ToDo initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { CreateTodo } from './components/CreateTodo';
 import { Todo } from './components/Todos';
 
 function App() {
+  const [todos,setTodos]=useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:4000/todos").then(async function(res) {
+      const json=await res.json();
+      setTodos(json.todos)
+    })
+  },[])
+  
   return (
     <div className="App">
       hi there
       <CreateTodo/>
-      <Todo/>
+      <Todo todos={todos}/>
     </div>
   );
 }
--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,18 +1,45 @@
+import { useState } from "react"
+
 export function CreateTodo(){
+    const [title,setTitle]=useState("");
+    const [description,setDescription]=useState("");
+
+    const addToDo=()=>{
+            fetch("http://localhost:4000/newTodo",{
+                method:"POST",
+                body:JSON.stringify({title:title,description:description}),
+                headers:{"Content-Type":"application/json"}
+            }).then(async function(res){
+                    // const json=await res.json();
+                    alert("Todo Added");
+            })
+        
+    }
+        
+    const changeTitle=(e)=>{
+        setTitle(e.target.value);
+    }
+    const changeDesc=(e)=>{
+        setDescription(e.target.value);
+    }
     return <div>
         <input style={{
             padding:10,
             margin:10
         }}
+        
+        onChange={changeTitle}
          type="text" placeholder="title" /><br/>
         <input style={{
             padding:10,
             margin:10
         }}
+        
+        onChange={changeDesc}
          type="text"  placeholder="description"/><br/>
         <button style={{
             padding:10,
             margin:10
-        }}>Add a todo</button>
+        }} onClick={addToDo}>Add a todo</button>
     </div>
 }
