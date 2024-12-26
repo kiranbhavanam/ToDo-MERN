68c039a8d872acded0bed09eb0a39eae4f55b935
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-12-26 16:06:56 +0530

    mongodb code

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@
 
 const express=require("express");
 const { newTodoSchema,updateTodoSchema } = require("./types");
+const {todo} =require("./db")
 const app=express();//initializes empty express app.
 app.use(express.json());//make sures all the post endpoints will work,parse the body if its json body.
 //Authentication for signup and signin
@@ -10,8 +11,11 @@ app.post("/signup",(req,res)=>{})
 app.post("/signin",(req,res)=>{})
 
 // input validation for all the routes.
-app.get("/todos",(req,res)=>{})
-app.post("/newTodo",(req,res)=>{
+app.get("/todos",async (req,res)=>{
+    const todos=await todo.find({});
+    res.json({todos})
+})
+app.post("/newTodo",async (req,res)=>{
     const createPayLoad=req.body;
     const parsePayLoad=newTodoSchema.safeParse(createPayLoad);
     if(!parsePayLoad.success){
@@ -21,8 +25,16 @@ app.post("/newTodo",(req,res)=>{
         return;
     }
     // put it in the mongoDB.
+   await todo.create({
+        title:createPayLoad.title,
+        description:createPayLoad.description,
+        completed:false
+    })
+    res.json({
+        msg:"Todo Created"
+    })
 })
-app.put("updateToDo",(req,res)=>{
+app.put("updateToDo",async (req,res)=>{
     const updatePayLoad=req.body;
     const parsePayLoad=updateTodoSchema.safeParse(updatePayLoad);
     if(!parsePayLoad.success){
@@ -32,6 +44,10 @@ app.put("updateToDo",(req,res)=>{
         return;
     }
     //update todo in mongoDB.
+    await todo.update({_id:req.body.id },{completed:true})
+    res.json({
+        msg:"Todo marked as completed."
+    })
 })
 
 app.listen(3000,(error)=>{
